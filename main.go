94f13e54a7dbc07e4c18c2d88f72fdc31efe6dcf
Abhinav Pathak <abhipth@amazon.com>
2020-06-02 15:13:55 -0700

    Auto-generate Core Resource Controller
    Commands Run:
    - kubebuilder edit --multigroup=true
    - kubebuilder create api --group "core" --kind "Pod" --version "v1" [controller only]
    - kubebuilder create api --group "core" --kind "Node" --version "v1" [controller only]

--- main.go
+++ main.go
@@ -20,11 +20,14 @@ import (
 	"flag"
 	"os"
 
+	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+
+	corecontroller "amazon-vpc-resource-controller-k8s/controllers/core"
 	// +kubebuilder:scaffold:imports
 )
 
@@ -36,6 +39,7 @@ var (
 func init() {
 	_ = clientgoscheme.AddToScheme(scheme)
 
+	_ = corev1.AddToScheme(scheme)
 	// +kubebuilder:scaffold:scheme
 }
 
@@ -62,6 +66,22 @@ func main() {
 		os.Exit(1)
 	}
 
+	if err = (&corecontroller.PodReconciler{
+		Client: mgr.GetClient(),
+		Log:    ctrl.Log.WithName("controllers").WithName("Pod"),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "Pod")
+		os.Exit(1)
+	}
+	if err = (&corecontroller.NodeReconciler{
+		Client: mgr.GetClient(),
+		Log:    ctrl.Log.WithName("controllers").WithName("Node"),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "Node")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	setupLog.Info("starting manager")
