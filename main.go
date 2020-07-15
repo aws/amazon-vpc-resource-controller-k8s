f3bb95e8a2827b24333a679d7abb0ef8bb897354
Hao Zhou <zhuhz@amazon.com>
2020-07-15 15:13:59 -0700

    Added health check endpoint into controller

--- main.go
+++ main.go
@@ -32,6 +32,7 @@ import (
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
 	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
@@ -120,12 +121,13 @@ func main() {
 
 	config := ctrl.GetConfigOrDie()
 	mgr, err := ctrl.NewManager(config, ctrl.Options{
-		SyncPeriod:         &syncPeriod,
-		Scheme:             scheme,
-		MetricsBindAddress: metricsAddr,
-		Port:               9443,
-		LeaderElection:     enableLeaderElection,
-		LeaderElectionID:   "bb6ce178.k8s.aws",
+		SyncPeriod:             &syncPeriod,
+		Scheme:                 scheme,
+		MetricsBindAddress:     metricsAddr,
+		Port:                   9443,
+		LeaderElection:         enableLeaderElection,
+		LeaderElectionID:       "bb6ce178.k8s.aws",
+		HealthProbeBindAddress: ":61779", // the liveness endpoint is default to "/healthz"
 	})
 	if err != nil {
 		setupLog.Error(err, "unable to start manager")
@@ -142,6 +144,14 @@ func main() {
 	clientSet, err := kubernetes.NewForConfig(config)
 	if err != nil {
 		setupLog.Error(err, "failed to create client set")
+		os.Exit(1)
+	}
+
+	// Add liveness probe
+	err = mgr.AddHealthzCheck("health-ping", healthz.Ping)
+	setupLog.Info("adding health check for controller")
+	if err != nil {
+		setupLog.Error(err, "unable add a health check")
 		os.Exit(1)
 	}
 
