983db501200876bd6736098145a882054a5e46e1
Hao Zhou <zhuhz@amazon.com>
2022-05-19 19:18:30 +0000

    Adding regression e2e tests for node validating webhook.
    SIM: https://i.amazon.com/EKS-NW-3131
    cr: https://code.amazon.com/reviews/CR-69776265

--- go.mod
+++ go.mod
@@ -40,6 +40,7 @@ require (
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/matttproud/golang_protobuf_extensions v1.0.2-0.20181231171920-c182affec369 // indirect
+	github.com/moby/spdystream v0.2.0 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/nxadm/tail v1.4.8 // indirect
--- test/integration/webhook/validating_webhook_test.go
+++ test/integration/webhook/validating_webhook_test.go
@@ -22,6 +22,7 @@ import (
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	v1 "k8s.io/api/rbac/v1"
 )
 
 // TODO: Add integration test for Windows when ConfigMap feature is implemented.
@@ -83,4 +84,139 @@ var _ = Describe("when doing pod operations from non vpc-resource-controller use
 			Expect(err).ToNot(HaveOccurred())
 		})
 	})
+
+	Context("[CANARY] when updating node", func() {
+		labelKey := "k8s-app"
+		labelCNIValue := "aws-node"
+		labelProxyValue := "kube-proxy"
+		cmdCurl := []string{
+			"curl",
+			"-o",
+			"kubectl",
+			"https://s3.us-west-2.amazonaws.com/amazon-eks/1.22.6/2022-03-09/bin/linux/amd64/kubectl",
+		}
+		cmdChMod := []string{
+			"chmod", "+x", "./kubectl",
+		}
+
+		var awsNodeOldRules []v1.PolicyRule
+		var kubeProxyOldRules []v1.PolicyRule
+		It("should successfully update clusterroles", func() {
+			awsNodeOldRules = PatchClusterRole("aws-node", nil, "nodes", []string{"get", "list", "watch", "update", "patch"})
+			kubeProxyOldRules = PatchClusterRole("system:node-proxier", nil, "nodes", []string{"get", "list", "watch", "update", "patch"})
+		})
+
+		It("should fail on updating node with unauthorized taint from aws-node", func() {
+			awsNodePods, err := frameWork.PodManager.GetPodsWithLabel(ctx, config.KubeSystemNamespace, labelKey, labelCNIValue)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(awsNodePods).ToNot(BeEmpty())
+
+			nodes, err := frameWork.NodeManager.GetNodesWithOS(config.OSLinux)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(nodes.Items).ToNot(BeEmpty())
+
+			awsNodePodName := awsNodePods[0].ObjectMeta.Name
+			targetNode := awsNodePods[1].Spec.NodeName
+			cmdTaint := []string{
+				"./kubectl",
+				"taint",
+				"no",
+				targetNode,
+				"key1=value1:NoSchedule",
+			}
+
+			cmdLabel := []string{
+				"./kubectl",
+				"label",
+				"no",
+				targetNode,
+				"test=true",
+			}
+
+			for i, cmd := range [][]string{cmdCurl, cmdChMod, cmdTaint, cmdLabel} {
+				stdout, stderr, err := frameWork.PodManager.PodExec(config.KubeSystemNamespace, awsNodePodName, cmd)
+				if i > 1 {
+					Expect(err).To(HaveOccurred())
+					Expect(stdout).To(BeEmpty())
+					Expect(stderr).To(ContainSubstring("denied the request"))
+				} else {
+					Expect(err).ToNot(HaveOccurred())
+				}
+			}
+		})
+
+		It("Should succeed on updating node with labels from non aws-node", func() {
+			kubeProxyPods, err := frameWork.PodManager.GetPodsWithLabel(ctx, config.KubeSystemNamespace, labelKey, labelProxyValue)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(kubeProxyPods).ToNot(BeEmpty())
+
+			nodes, err := frameWork.NodeManager.GetNodesWithOS(config.OSLinux)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(nodes.Items).ToNot(BeEmpty())
+			kubeProxyPodName := kubeProxyPods[0].ObjectMeta.Name
+			cmdUpdateApt := []string{
+				"apt", "update",
+			}
+			cmdInstallCurl := []string{
+				"apt", "install", "curl", "-y",
+			}
+			targetNode := kubeProxyPods[1].Spec.NodeName
+			cmdLabel := []string{
+				"./kubectl",
+				"label",
+				"no",
+				targetNode,
+				"test=true",
+			}
+
+			for i, cmd := range [][]string{cmdUpdateApt, cmdInstallCurl, cmdCurl, cmdChMod, cmdLabel} {
+				stdout, stderr, err := frameWork.PodManager.PodExec(config.KubeSystemNamespace, kubeProxyPodName, cmd)
+				Expect(err).ToNot(HaveOccurred())
+				if i == 4 {
+					Expect(stdout).To(ContainSubstring("labeled"))
+					Expect(stderr).To(BeEmpty())
+				}
+			}
+
+			// remove the test label from the node
+			cmdLabel = []string{
+				"./kubectl",
+				"label",
+				"no",
+				targetNode,
+				"test-",
+			}
+			stdout, stderr, err := frameWork.PodManager.PodExec(config.KubeSystemNamespace, kubeProxyPodName, cmdLabel)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(stdout).To(ContainSubstring("labeled"))
+			Expect(stderr).To(BeEmpty())
+		})
+
+		It("Should succeed on restoring clusterroles", func() {
+			awsNodeOldRules = PatchClusterRole("aws-node", awsNodeOldRules, "nodes", []string{"get", "list", "watch", "update", "patch"})
+			kubeProxyOldRules = PatchClusterRole("system:node-proxier", kubeProxyOldRules, "nodes", []string{"get", "list", "watch", "update", "patch"})
+		})
+	})
 })
+
+func PatchClusterRole(roleName string, rules []v1.PolicyRule, resourceName string, verbs []string) []v1.PolicyRule {
+	role, err := frameWork.RBACManager.GetClusterRole(roleName)
+	Expect(err).ToNot(HaveOccurred())
+	newRole := role.DeepCopy()
+
+	if rules != nil {
+		newRole.Rules = rules
+	} else {
+		for i, rule := range newRole.Rules {
+			for _, resource := range rule.Resources {
+				if resource == resourceName {
+					rule.Verbs = verbs
+				}
+			}
+			newRole.Rules[i] = rule
+		}
+	}
+	err = frameWork.RBACManager.PatchClusterRole(newRole)
+	Expect(err).ToNot(HaveOccurred())
+	return role.Rules
+}
