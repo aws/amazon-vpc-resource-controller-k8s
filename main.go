8d247472ec222be0ac51ea11f81a114ffc33e227
Jyoti Mahapatra <jyotima@amazon.com>
2022-10-17 22:15:54 -0700

    multi lock

--- main.go
+++ main.go
@@ -216,7 +216,7 @@ func main() {
 		RetryPeriod:                &retryPeriod,
 		LeaderElectionID:           config.LeaderElectionKey,
 		LeaderElectionNamespace:    config.LeaderElectionNamespace,
-		LeaderElectionResourceLock: resourcelock.LeasesResourceLock,
+		LeaderElectionResourceLock: resourcelock.ConfigMapsLeasesResourceLock,
 		HealthProbeBindAddress:     ":61779", // the liveness endpoint is default to "/healthz"
 		NewCache:                   newCache,
 	})
